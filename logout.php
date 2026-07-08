137e4f90dd07c7c7f690001e356ee082551a2552
Faten1231 <73039565+Faten1231@users.noreply.github.com>
2026-07-08 14:26:57 +0200

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+// Je supprime toutes les informations de session
+session_destroy();
+
+// Je redirige l'utilisateur vers la page de connexion
+header('Location: /sport-news-crud/login.php');
+exit;
