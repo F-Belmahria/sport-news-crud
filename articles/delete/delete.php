faf48415a61705b13a5d3d7702c1a0bed8c66a55
Faten1231 <73039565+Faten1231@users.noreply.github.com>
2026-07-08 14:29:38 +0200

    Update delete.php

--- articles/delete/delete.php
+++ articles/delete/delete.php
@@ -1,4 +1,12 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['LOGGED_USER'])) {
+    header('Location: /sport-news-crud/login.php');
+    exit;
+}
 require_once '../../config/database.php';
 
 $getData = $_GET;
