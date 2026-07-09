9f7ae36e166872aa863ccbac51aa9e6d844b2ae4
Faten1231 <73039565+Faten1231@users.noreply.github.com>
2026-07-09 14:44:46 +0200

    Update store.php

--- articles/create/store.php
+++ articles/create/store.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $titre = $_POST['titre'];
     $contenu = $_POST['contenu'];
     $auteur = $_POST['auteur'];
-    $image = $_POST['image'];
+    
     $date_publication = $_POST['date_publication'];
     $match_id = $_POST['match_id'];
 
@@ -24,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         empty($titre) ||
         empty($contenu) ||
         empty($auteur) ||
-        empty($image) ||
+       
         empty($date_publication) ||
         empty($match_id)
     ) {
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $titre = trim(strip_tags($titre));
     $contenu = trim(strip_tags($contenu));
     $auteur = trim(strip_tags($auteur));
-    $image = trim(strip_tags($image));
+    $image =  'default.png';
 
     // Je prépare la requête SQL avec des placeholders
     $sql = "INSERT INTO articles 
@@ -58,7 +58,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Je lie l'article au match sélectionné grâce à la clé étrangère match_id
         'match_id' => $match_id
     ]);
+// Je récupère l'id de l'article qui vient d'être ajouté
+// Cet id va servir à renommer l'image
+$article_id = $pdo->lastInsertId();
 
+
+// ============================================================
+// TRAITEMENT DE L'UPLOAD D'IMAGE
+// ============================================================
+
+// Je vérifie qu'une image a été envoyée et qu'il n'y a pas d'erreur
+if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+
+    // Je vérifie le vrai type du fichier
+    $typeMime = mime_content_type($_FILES['image']['tmp_name']);
+
+    // J'accepte seulement les images JPG / JPEG ou png
+   if ($typeMime === "image/jpeg" || $typeMime === "image/png") {
+
+        // Je choisis le dossier où l'image sera enregistrée
+        $dossier = "../../assets/images/";
+
+        // Je donne un nom à l'image avec l'id de l'article
+        // Exemple : si l'article a l'id 5, l'image sera 5.jpg
+        $nomFichier = $article_id . ".jpg";
+
+        // Je déplace l'image dans le dossier assets/images
+        if (move_uploaded_file($_FILES['image']['tmp_name'], $dossier . $nomFichier)) {
+
+            // Je mets à jour l'article avec le vrai nom de l'image
+            $updateImage = $pdo->prepare("
+                UPDATE articles 
+                SET image = :image 
+                WHERE id = :id
+            ");
+
+            $updateImage->execute([
+                'image' => $nomFichier,
+                'id' => $article_id
+            ]);
+        }
+
+    } else {
+        echo "Erreur : seul le format JPG est accepté.";
+        exit;
+    }
+}
     // Après l'ajout, je retourne vers la page qui affiche les articles
     header('Location: ../read/index.php');
     exit;
