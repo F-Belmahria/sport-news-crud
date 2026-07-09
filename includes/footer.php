4e81c129e96d82c56c1f5ee90d661400d98005e5
Faten1231 <73039565+Faten1231@users.noreply.github.com>
2026-07-09 11:51:33 +0200

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -11,23 +11,14 @@
                 </p>
             </div>
 
-            <!-- Liens -->
+           
             <div class="col-12 col-md-4 mb-3 mb-md-0 text-md-center">
-                <a href="/sport-news-crud/" class="text-white text-decoration-none me-3">
-                    Articles
-                </a>
-
-                <a href="/sport-news-crud/articles/create/create.php" class="text-white text-decoration-none">
-                    Ajouter un article
-                </a>
-            </div>
-
-            <!-- Copyright -->
-            <div class="col-12 col-md-4 text-md-end">
-                <p class="mb-0 text-white-50">
-                    © <?= date('Y') ?> - Projet CRUD PHP
+               <p class="mb-0 text-white-50">
+                    <?= date('Y') ?> - Projet CRUD PHP
                 </p>
             </div>
+
+            
 
         </div>
 
